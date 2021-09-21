abc026d7065b2fc7f01d56dc86a7560e60a852e6
jhackmeyer <jhackmeyer@ucla.edu>
2021-09-21 01:26:15 -0700

    Editing formatting and adding popup

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -26,7 +26,7 @@ let baseMaps = {
 let map = L.map('mapid', {
     center : [43.7, -79.3],
     zoom: 11,
-    layers: [satelliteStreets]
+    layers: [streets]
 });
 
 
@@ -35,18 +35,25 @@ L.control.layers(baseMaps).addTo(map)
 
 
 // Accessing the airport GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/jhackmeyer/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+let torontoHoods = "https://raw.githubusercontent.com/jhackmeyer/Mapping_Earthquakes/master/torontoNeighborhoods.json";
 
-// let myStyle = {
-//     color: "#ffffa1",
-//     weight:2};
+let myStyle = {
+    color: "blue",
+    weight: 1,
+    fillColor: "yellow"};
 
 
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function(data) {
     console.log(data);
-
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+    
+    L.geoJson(data, {
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            console.log(layer);
+            layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+        }
+}).addTo(map);
 });
-
+    
+    
